2e3d6dc7d1b595dda5fe1c63aad855e54be91a2d
menslow <michael.enslow@gmail.com>
2014-11-04 22:46:35 -0800

    imported foundation and basic patterns

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 mm-pattern-library
 ==================
 
-Mister Machine Pattern Library
+## Mister Machine Pattern Library
+
+A starter project for front-end pattern libraries.
+
+Built with Middleman and Foundation.
--- config.rb
+++ config.rb
@@ -7,6 +7,35 @@
 #   config.output_style = :compact
 # end
 
+compass_config do |config|
+    # Require any additional compass plugins here.
+    config.add_import_path "bower_components/foundation/scss"
+    
+    # Set this to the root of your project when deployed:
+    config.http_path = "/"
+    config.css_dir = "stylesheets"
+    config.sass_dir = "stylesheets"
+    config.images_dir = "images"
+    config.javascripts_dir = "javascripts"
+    
+    # You can select your preferred output style here (can be overridden via the command line):
+    # output_style = :expanded or :nested or :compact or :compressed
+    
+    # To enable relative paths to assets via compass helper functions. Uncomment:
+    # relative_assets = true
+    
+    # To disable debugging comments that display the original location of your selectors. Uncomment:
+    # line_comments = false
+    
+    
+    # If you prefer the indented syntax, you might want to regenerate this
+    # project again passing --syntax sass, or you can uncomment this:
+    # preferred_syntax = :sass
+    # and then run:
+    # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+    
+end
+
 ###
 # Page options, layouts, aliases and proxies
 ###
