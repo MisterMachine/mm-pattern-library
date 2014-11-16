d709f7d5f9da145a2c020f1d0448265cc84dd110
slowbot <jesse@slowbot.com>
2014-11-15 23:55:17 -0800

    Updating layout template and type library

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,7 +3,11 @@
 //= require foundation/js/foundation/foundation.offcanvas
 //= require foundation/js/foundation/foundation.reveal
 //= require foundation/js/foundation/foundation.dropdown
+//= require foundation/js/foundation/foundation.alert
+//= require foundation/js/foundation/foundation.abide
+//= require foundation/js/foundation/foundation.equalizer
 //= require google-code-prettify/bin/prettify.min.js
+//= require responsive-nav.js
 //= require jquery-scrolltofixed.js
 //= require angular/angular
 //= require app
